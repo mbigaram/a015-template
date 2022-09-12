e8d2056d7899e01eba47bb772513bfa3465acd2e
mbiga <mbigaram.mb@gmail.com>
2022-09-12 12:16:16 -0300

    pratica-guiada

---
+++ script.js
@@ -0,0 +1,48 @@
+
+//Exercicio 1
+
+
+// let num 
+
+// let soma = 0
+
+
+// while(num !== 0){   
+    
+// 	num = Number(prompt("Qual o numero?"))
+//     soma = soma + num
+   
+//     console.log (soma)
+
+// }
+
+//Exercicio 2
+
+// let num = Number(prompt("Qual o numero?"))
+
+// for(let i = 0; i <= num; i++){
+// 	console.log(i);
+// }
+
+// Exercicio 3
+
+// const numeros = [45, 87, 11, 7, 100, 48, 2]
+
+// for(let i = 0; i < numeros.length; i++){
+// 	console.log("O numero do indice "+i,"é "+ numeros[i]);
+// }
+
+
+// Pra guardar na cabeça
+
+let tipou = (prompt("Qual seu tipo de usuario?\nA?\nB?\nC?").toUpperCase())
+
+while(tipou !== "A"){  
+    alert("Acesso negado")
+	tipou = (prompt("Qual seu tipo de usuario?\nA?\nB?\nC?").toUpperCase())
+
+}    
+    console.log ("Boas vindas admim!")
+
+    
+
